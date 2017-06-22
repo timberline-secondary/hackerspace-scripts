3b33e3248a953f66f140dcef11cc61e89b1aff2f
PederSkaaravik <PederSkaaravik@gmail.com>
2017-06-22 13:58:13 -0700

    enhanced theme script close #22 close #15

--- bin/Entrance_Themes/add-theme.sh
+++ bin/Entrance_Themes/add-theme.sh
@@ -3,11 +3,24 @@ USERNAME=pi
 HOSTNAME=pi-themes
 LOCATION="/media/THEMES"
 
-echo "Paste the link to the mp3 file you want to add"
-read url
+echo "Paste the link or directory to the mp3 file you want to add"
+read linkdir
 echo "What number do you want to give it?"
 read filename
 
-SCRIPT="cd $LOCATION; wget -O $filename.mp3 $url"
+if [[ ${linkdir:0:2} == "~/" ]]
+then
+linkdir="${linkdir#'~'}"
+linkdir=/home/$USER$linkdir
+echo "$linkdir"
+fi
 
-ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+if [[ ${linkdir:0:1} == "/" ]] || [[ ${linkdir:0:2} == "~/" ]]
+then
+  echo "Sending local file here@here"
+  scp $linkdir ${USERNAME}@${HOSTNAME}:$LOCATION/$filename.mp3
+else
+  SCRIPT="cd $LOCATION; wget -O $filename.mp3 $linkdir"
+
+  ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+fi
