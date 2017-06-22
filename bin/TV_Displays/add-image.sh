81f1153c5e1a43625c304a7c6db33e4aff307ccb
PederSkaaravik <PederSkaaravik@gmail.com>
2017-06-22 14:37:15 -0700

    allow blank names to keep orriginal file name. close #16

--- bin/TV_Displays/add-image.sh
+++ bin/TV_Displays/add-image.sh
@@ -4,22 +4,18 @@ WARNING='\033[1;33m'
 FEEDBACK='\033[0;36m'
 #create tmp folder for the image, will be removed in the end.
 
-
 smbclient_check=$(dpkg -l | grep "ii  smbclient")
 echo $smbclient_check
-if [[ $smbclient_check = "" ]]
-then
+if [[ $smbclient_check == "" ]]; then
   echo "You are missing packages that are required to run this script, hackerspace_admin password required to install:"
   cd ..
-su -c "sudo -S ./.dialoginstall.sh" -m hackerspace_admin
+  su -c "sudo -S ./.dialoginstall.sh" -m hackerspace_admin
   exit
 fi
 
-
- tmpfolder=$(pwd)/tmpimage
- if [ -e "$tmpfolder" ]
- then
-rm -r ./tmpimage
+tmpfolder=$(pwd)/tmpimage
+if [ -e "$tmpfolder" ]; then
+  rm -r ./tmpimage
 fi
 mkdir ./tmpimage
 cd ./tmpimage
@@ -27,33 +23,33 @@ cd ./tmpimage
 echo -e "${COLOR}Paste your image link(${WARNING}must be https link${COLOR}), or specify exact local directory(${WARNING}must begin with "/"${COLOR}):${RESTORE}"
 read image_link
 #chane "~" to "/home/$USER"
-if [[ ${image_link:0:2} == "~/" ]]
-then
-linkdir="${image_link#'~'}"
-linkdir=/home/$USER$image_link
+if [[ ${image_link:0:2} == "~/" ]]; then
+  linkdir="${image_link#'~'}"
+  linkdir=/home/$USER$image_link
 fi
 #give feedback based on what is submitted by user, a file, or link.
-if [[ ${image_link:0:4} == "http" ]]
-then
+if [[ ${image_link:0:4} == "http" ]]; then
   echo -e "${FEEDBACK} IMAGE LINK DETECTED! ${RESTORE}"
 fi
 
-if [[ ${image_link:0:1} == "/" ]]
-then
+if [[ ${image_link:0:1} == "/" ]]; then
   echo -e "${FEEDBACK} FILE DETECTED! ${RESTORE}"
 fi
-if [[ ${image_link:0:2} == "~/" ]]
-then
+if [[ ${image_link:0:2} == "~/" ]]; then
   echo -e "${FEEDBACK} FILE DETECTED! ${RESTORE}"
 fi
 # check extension for later percautions and to avoid file corruption.
 url_filename=$(basename "$image_link")
 extension="${url_filename##*.}"
+original_linkname_with_extension="${image_link##*/}"
+original_linkname="${original_linkname_with_extension%.*}"
+
+
 
 echo -e "${COLOR}Student Number:${RESTORE}"
 read student_number
 
-echo -e "${COLOR}Name your image (ex. Mountains):${RESTORE}"
+echo -e "${COLOR}Name your image (ex. Mountains) or press enter to keep orriginal:${RESTORE}"
 read image_name
 
 echo -e "${COLOR}Which TV? (1-4):${RESTORE}"
@@ -61,20 +57,24 @@ read tv
 #declare filename format
 filename="${student_number}.z.${image_name}"
 #Get the image from online, or copy local file to local directory.
-if [[ ${image_link:0:4} == "http" ]]
-then
+if [[ ${image_link:0:4} == "http" ]]; then
+
   wget -O $image_name.$extension $image_link
 fi
-if [[ ${image_link:0:1} == "/" ]]
-then
+if [[ ${image_link:0:1} == "/" ]]; then
   cp $image_link ./$image_name.$extension
 fi
-if [[ ${image_link:0:1} == "~/" ]]
-then
+if [[ ${image_link:0:1} == "~/" ]]; then
   cp $image_link ./$image_name.$extension
 fi
 #rename file to designated filename
-cp ./$image_name.$extension ./$filename.$extension
+if [[ $image_name == "" ]]; then
+  original_linkname_with_extension="${image_link##*/}"
+  original_linkname="${original_linkname_with_extension%.*}"
+  cp ./$original_linkname_with_extension ./$filename.$extension
+else
+  cp ./$image_name.$extension ./$filename.$extension
+fi
 #cp $filename $filename.png
 echo "$filename will now be uploaded, enter desired password:"
 smbclient //hightower/tv$tv -W hightower -U pi-slideshow -c "put ${filename}.$extension"
