b3b5698992cce3cf57a78a30866eb74e73bc17de
PederSkaaravik <PederSkaaravik@gmail.com>
2017-06-21 17:23:21 -0700

    stage one launches stage 2. close #8

--- bin/Miscellaneous/launch.sh
+++ bin/Miscellaneous/launch.sh
@@ -4,4 +4,4 @@ echo "* Activating Missile Launcher *"
 echo "*******************************"
 
 # how can we just put the script here instead of a seperate file?
-su -c "sudo -S .launchStage2.sh" -m hackerspace_admin
+su -c "sudo -S ./.launchStage2.sh" -m hackerspace_admin
