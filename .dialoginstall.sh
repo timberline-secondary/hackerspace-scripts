df05e62587e4269cbf92dee044b79cf94c7de2d5
PederSkaaravik <pederskaaravik@gmail.com>
2017-11-20 23:47:37 -0800

    comment number 1

--- .dialoginstall.sh
+++ .dialoginstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Attempting to install `dialog` package"
 sudo apt install dialog beep smbclient
+#comment number one
 #beep is literally just a package that adds the command "beep" to play a beep sound when executed (for my microwave :D)
